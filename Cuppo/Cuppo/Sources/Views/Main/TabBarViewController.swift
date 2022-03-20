ef80c6282f243d8ab4d17b5ba10ac2fce1510c62
LeeHa-Yeon <gkdus1613@naver.com>
2022-03-21 01:19:54 +0900

    [FEAT] 공통작업 부분인 TabBarVC 파일 추가

---
+++ Cuppo/Cuppo/Sources/Views/Main/TabBarViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarViewController.swift
+//  Cuppo
+//
+//  Created by 이하연 on 2022/03/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
