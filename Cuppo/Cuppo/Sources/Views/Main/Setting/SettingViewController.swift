9c4ff09a5777d71cb3d73ffc3911e86143483729
Teddy <teddy@swit.io>
2022-03-22 23:48:05 +0900

    [ADD] Setting View 스토리보드 연결
    SettingNaviVC 를 루트로 Setting View의 루트를 네비게이션 뷰컨으로 세팅

--- Cuppo/Cuppo/Sources/Views/Main/Setting/SettingViewController.swift
+++ Cuppo/Cuppo/Sources/Views/Main/Setting/SettingViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
 class SettingViewController: BaseController {
 
+    lazy var testButton = UIButton().then{
+        $0.setTitle("전환", for: .normal)
+        $0.setTitleColor(.systemBackground, for: .normal)
+        $0.backgroundColor = .black
+        $0.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+    }
+    
+    let testLable = UILabel().then{
+        $0.text = "adsasdasdjasdasdjaskdas"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.view.backgroundColor = .systemBackground
+        
+        setLayout()
     }
-
+    
+    func setLayout(){
+        self.view.addSubview(testButton)
+        self.view.addSubview(testLable)
+        
+        testButton.snp.makeConstraints{
+            $0.top.equalTo(self.view.safeAreaLayoutGuide).offset(32.0)
+            $0.leading.equalToSuperview().offset(120.0)
+            $0.trailing.equalToSuperview().offset(-120.0)
+            $0.height.equalTo(60.0)
+        }
+        
+        testLable.snp.makeConstraints{
+            $0.top.equalTo(testButton.snp.bottom).offset(32.0)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    @objc
+    func buttonTapped(_ sender: UIButton){
+        let testVC = SettingTwoViewController()
+        
+        self.navigationController?.pushViewController(testVC, animated: true)
+    }
 }
