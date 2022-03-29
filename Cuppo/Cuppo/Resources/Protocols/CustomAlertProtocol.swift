1181cbfaf473930a35675d1e4edc6d2deed0c68d
LeeHa-Yeon <gkdus1613@naver.com>
2022-03-29 19:12:08 +0900

    [FEAT] Custom Alert창 구현

---
+++ Cuppo/Cuppo/Resources/Protocols/CustomAlertProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CustomAlertProtocol.swift
+//  Cuppo
+//
+//  Created by 이하연 on 2022/03/29.
+//
+
+import UIKit
+
+protocol CustomAlertProtocol {
+    func cancleButtonTapped(_ sender: UIButton,_ test: UIView)
+    func okButtonTapped(_ sender: UIButton, _ test: UIView)
+}
